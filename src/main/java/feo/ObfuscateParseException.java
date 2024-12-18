e4ddc1db68dd72ca98b9cfc91ffb49e04ec0a21f
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2024-12-18 11:24:44 +0300

    Error message

--- src/main/java/feo/ObfuscateParseException.java
+++ src/main/java/feo/ObfuscateParseException.java
@@ -1,4 +1,7 @@
 package feo;
 
 public class ObfuscateParseException extends RuntimeException {
+    public ObfuscateParseException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/main/java/feo/ObfuscateTool.java
+++ src/main/java/feo/ObfuscateTool.java
@@ -1,22 +1,27 @@
 package feo;
 
 import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 import java.io.BufferedWriter;
 
 public class ObfuscateTool {
+    private static class ErrorListener extends BaseErrorListener {
+        @Override
+        public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+            throw new ObfuscateParseException("line " + line + ":" + charPositionInLine + " " + msg, e);
+        }
+    }
+
     public static void obfuscate(final CharStream in, final BufferedWriter out) {
         final ProgramLexer lexer = new ProgramLexer(in);
+        final ErrorListener listener = new ErrorListener();
         lexer.removeErrorListeners();
+        lexer.addErrorListener(listener);
         final CommonTokenStream tokens = new CommonTokenStream(lexer);
         final ProgramParser parser = new ProgramParser(tokens);
-        parser.setErrorHandler(new BailErrorStrategy());
-        try {
-            final var program = parser.program();
-            new ObfuscateVisitor(out).visit(program);
-        } catch (final ParseCancellationException e) {
-            throw new ObfuscateParseException();
-        }
+        parser.removeErrorListeners();
+        parser.addErrorListener(listener);
+        final var program = parser.program();
+        new ObfuscateVisitor(out).visit(program);
     }
 }
