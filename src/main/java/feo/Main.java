673f3f85072bb2175e093f7fc26a6c2aeadabe7f
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2024-12-18 17:15:19 +0300

    Main

--- src/main/java/feo/Main.java
+++ src/main/java/feo/Main.java
@@ -7,25 +7,28 @@ import java.io.IOException;
 import java.io.StringWriter;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-//        CharStream in = CharStreams.fromString("int main () { 1 }");
-        var in = CharStreams.fromFileName("test.cpp");
-//        CharStream in = CharStreams.fromString("{ int a = 1; if (1) { int b = 5; } else return 6; }");
-        ProgramLexer lexer = new ProgramLexer(in);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ProgramParser parser = new ProgramParser(tokens);
-        var prg = parser.program();
-        System.out.println(prg.toStringTree());
-        System.out.println(tokens.getText());
-        var s = new StringWriter();
-//        System.out.println(lexer.error);
-//        System.out.println(parser.getNumberOfSyntaxErrors());
-        BufferedWriter w = new BufferedWriter(s);
-        System.out.println("Visiting");
-        new ObfuscateVisitor(w).visit(prg);
-//        ObfuscateTool.obfuscate(in, w);
-        w.flush();
-//        System.out.println("Visited");
-        System.out.println(s);
+    public static void main(final String[] args) {
+        if (args.length != 1) {
+            System.err.println("Specify input file name");
+            return;
+        }
+        final CharStream in;
+        try {
+            in = CharStreams.fromFileName(args[0]);
+        } catch (final IOException e) {
+            System.err.println("Error during reading input file: " + e.getMessage());
+            return;
+        }
+        final StringWriter string = new StringWriter();
+        final BufferedWriter writer = new BufferedWriter(string);
+        try {
+            ObfuscateTool.obfuscate(in, writer);
+            writer.flush();
+            System.out.println(string);
+        } catch (final ObfuscateParseException e) {
+            System.err.println("Parsing error: " + e.getMessage());
+        } catch (final IOException e) {
+            System.err.println("Unexpected IO error: " + e.getMessage());
+        }
     }
 }
