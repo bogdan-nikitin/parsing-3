5f5dbcacc8d06bd37030c5c75fb6edbe865d6d69
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2024-12-18 11:25:32 +0300

    Reformat

--- src/main/java/feo/ObfuscateTool.java
+++ src/main/java/feo/ObfuscateTool.java
@@ -5,13 +5,6 @@ import org.antlr.v4.runtime.*;
 import java.io.BufferedWriter;
 
 public class ObfuscateTool {
-    private static class ErrorListener extends BaseErrorListener {
-        @Override
-        public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-            throw new ObfuscateParseException("line " + line + ":" + charPositionInLine + " " + msg, e);
-        }
-    }
-
     public static void obfuscate(final CharStream in, final BufferedWriter out) {
         final ProgramLexer lexer = new ProgramLexer(in);
         final ErrorListener listener = new ErrorListener();
@@ -23,5 +16,17 @@ public class ObfuscateTool {
         parser.addErrorListener(listener);
         final var program = parser.program();
         new ObfuscateVisitor(out).visit(program);
+    }
+
+    private static class ErrorListener extends BaseErrorListener {
+        @Override
+        public void syntaxError(final Recognizer<?, ?> recognizer,
+                                final Object offendingSymbol,
+                                final int line,
+                                final int charPositionInLine,
+                                final String msg,
+                                final RecognitionException e) {
+            throw new ObfuscateParseException("line " + line + ":" + charPositionInLine + " " + msg, e);
+        }
     }
 }
