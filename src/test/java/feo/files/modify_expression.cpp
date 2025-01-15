efe79745aa5cb7ad4f60dcf4fe8a98b829e24884
bogdan-nikitin <nikitin.bogdan333@yandex.ru>
2025-01-16 00:03:40 +0300

    Assignment instead of definition

--- src/test/java/feo/files/modify_expression.cpp
+++ src/test/java/feo/files/modify_expression.cpp
@@ -1,5 +1,5 @@
 int main() {
-    int a, b, c, d;
-    int x = 1 >= (a - (((3 + b++ / 5) == c) < 7)) * d;
+    int a, b, c, d, x;
+    x = 1 >= (a - (((3 + b++ / 5) == c) < 7)) * d;
     return 0;
 }
